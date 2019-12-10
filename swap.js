0dd931075ab530b3acd389847427c98216d4c5af
egar-pro <56841355+egar-pro@users.noreply.github.com>
2019-12-10 00:44:54 -0600

    update progress
    Big changes and features added

---
+++ swap.js
@@ -0,0 +1,30 @@
+function swapImage(img) {
+    if (img == document.getElementById('image1').src) {
+        //code to display imageName1.jpg in the larger image view
+        document.getElementById('myImage').src='images/train1.jpg';
+    } else if (img == document.getElementById('image2').src) {
+        //code to display imageName2.jpg in the larger image view
+        document.getElementById('myImage').src='images/train2.jpg';
+    } else if (img == document.getElementById('image3').src) {
+        //code to display imageName3.jpg in the larger image view
+        document.getElementById('myImage').src='images/train3.jpg';
+    } else if (img == document.getElementById('image4').src) {
+        //code to display imageName3.jpg in the larger image view
+        document.getElementById('myImage').src='images/train4.jpg';
+    } else if (img == document.getElementById('image5').src) {
+        //code to display imageName3.jpg in the larger image view
+        document.getElementById('myImage').src='images/train5.jpg';
+    } else if (img == document.getElementById('image6').src) {
+        //code to display imageName3.jpg in the larger image view
+        document.getElementById('myImage').src='images/train6.jpg';
+    } else if (img == document.getElementById('image7').src) {
+        //code to display imageName3.jpg in the larger image view
+        document.getElementById('myImage').src='images/train7.jpg';
+    } else if (img == document.getElementById('image8').src) {
+        //code to display imageName3.jpg in the larger image view
+        document.getElementById('myImage').src='images/train8.jpg';
+    } else if (img == document.getElementById('image9').src) {
+        //code to display imageName3.jpg in the larger image view
+        document.getElementById('myImage').src='images/train9.jpg';
+    }
+}
